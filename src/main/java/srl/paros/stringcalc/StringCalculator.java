46d9e648ef6288babd70eb4998cc4ce15bb53d8c
michele.pilloni <siorlu@gmail.com>
2017-06-07 18:17:08 +0200

    First commit

---
+++ src/main/java/srl/paros/stringcalc/StringCalculator.java
@@ -0,0 +1,12 @@
+package srl.paros.stringcalc;
+
+/**
+ * Created by michele.pilloni on 07/06/2017.
+ */
+public class StringCalculator {
+
+    public int add(String numbers) {
+        return -1;
+    }
+
+}
---
+++ src/test/java/srl/paros/stringcalc/StringCalculatorTest.java
@@ -0,0 +1,17 @@
+package srl.paros.stringcalc;
+
+import org.junit.Before;
+
+/**
+ * Created by michele.pilloni on 07/06/2017.
+ */
+
+public class StringCalculatorTest {
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void init() {
+        stringCalculator = new StringCalculator();
+    }
+
+}
