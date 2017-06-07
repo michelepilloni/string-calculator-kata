171d9cfd2c044c209bf41ce78f6516eef5d6c0b0
michele.pilloni <siorlu@gmail.com>
2017-06-07 18:25:28 +0200

    Step 1: test

--- src/test/java/srl/paros/stringcalc/StringCalculatorTest.java
+++ src/test/java/srl/paros/stringcalc/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 package srl.paros.stringcalc;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by michele.pilloni on 07/06/2017.
@@ -14,4 +17,29 @@ public class StringCalculatorTest {
         stringCalculator = new StringCalculator();
     }
 
+    @Test
+    public void whenEmptyStringReturnZero() {
+        int result = stringCalculator.add("");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void oneNumberAddition() {
+        int result = stringCalculator.add("1");
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void twoNumbersAddition() {
+        int result = stringCalculator.add("1,2");
+
+        assertEquals(1 + 2, result);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void whenMoreThanTwoParametersThrowException() throws Exception {
+        int result = stringCalculator.add("1,2,3");
+    }
 }
