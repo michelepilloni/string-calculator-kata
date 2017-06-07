345cd5f0639d39cf44e58d97028cc8fc8429bc74
michele.pilloni <siorlu@gmail.com>
2017-06-07 23:49:38 +0200

    Step 4: use case resolution

--- src/main/java/srl/paros/stringcalc/StringCalculator.java
+++ src/main/java/srl/paros/stringcalc/StringCalculator.java
@@ -5,11 +5,13 @@ package srl.paros.stringcalc;
  */
 public class StringCalculator {
 
+    private final static String DEFAULT_DELIMITERS = ",|\n";
+
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty())
             return 0;
 
-        String[] splitted = numbers.split(",|\n");
+        String[] splitted = getSplittedParameters(numbers);
 
         int result = 0;
 
@@ -18,6 +20,16 @@ public class StringCalculator {
             result += nr;
         }
 
+        return result;
+    }
+
+    private String[] getSplittedParameters(String parameters) {
+        if (!parameters.startsWith("//"))
+            return parameters.split(DEFAULT_DELIMITERS);
+
+        String delimiterPart = parameters.substring(parameters.indexOf("//") + "//".length(), parameters.indexOf("\n"));
+        String numbersPart = parameters.substring(parameters.indexOf("\n") + "\n".length());
+        String[] result = numbersPart.split(delimiterPart);
         return result;
     }
 
